880cb959f155252b432f6bae68da7cd2626a69a5
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-05-09 13:57:39 +0200

    ART-4 test pipeline errors

--- art/src/main/java/Main.java
+++ art/src/main/java/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Hello world!");
     }
+
+    public void test(int a) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
 }
