df5b9af7d930bb98ab1a74133e43c6467820b8be
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-05-09 14:20:28 +0200

    ART-4 test CI pipeline errors

--- art/src/main/java/Main.java
+++ art/src/main/java/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Hello world!");
     }
+
+    public void test() {
+        System.out.println("This is");
+        System.out.println("a test");
+        System.out.println("for CI!");
+    }
 }
