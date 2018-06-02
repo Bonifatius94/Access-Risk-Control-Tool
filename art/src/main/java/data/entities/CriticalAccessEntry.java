315aa5bb7a8c94fc312f9e108ce1ebdf6f2c4e22
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-06-02 15:51:13 +0200

    ART-42 added javadoc

--- art/src/main/java/data/entities/CriticalAccessEntry.java
+++ art/src/main/java/data/entities/CriticalAccessEntry.java
@@ -25,10 +25,21 @@ public class CriticalAccessEntry {
         return "ViolatedUseCaseID: " + authorizationPattern.getUsecaseId() + ", UserName: " + userName;
     }
 
-    public boolean equals(Object other){
-        if (other == null) return false;
-        if (other == this) return true;
-        if (!(other instanceof CriticalAccessEntry))return false;
+    /**
+     * Compares an entry to another entry using userName and useCaseID.
+     * @param other the object to compare with
+     * @return whether they are equal
+     */
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof CriticalAccessEntry)) {
+            return false;
+        }
         return (userName.equals(((CriticalAccessEntry) other).userName) && authorizationPattern.equals(((CriticalAccessEntry) other).getAuthorizationPattern()));
     }
 }
