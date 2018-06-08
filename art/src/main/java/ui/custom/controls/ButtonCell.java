8e10a6100a8d3e324867c78cf460e41c494b0df9
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-06-08 10:18:32 +0200

    ART-9 fixed compiler warnings (raw members) and errors

--- art/src/main/java/ui/custom/controls/ButtonCell.java
+++ art/src/main/java/ui/custom/controls/ButtonCell.java
@@ -12,7 +12,7 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 
-public class ButtonCell<S> extends TableCell<S, Button> {
+public class ButtonCell<S> extends TableCell<S, JFXButton> {
 
     private final JFXButton actionButton;
 
@@ -35,12 +35,12 @@ public class ButtonCell<S> extends TableCell<S, Button> {
         return (S) getTableView().getItems().get(getIndex());
     }
 
-    public static <S> Callback<TableColumn<S, Button>, TableCell<S, Button>> forTableColumn(MaterialDesignIcon icon, Function<S, S> function) {
+    public static <S> Callback<TableColumn<S, JFXButton>, TableCell<S, JFXButton>> forTableColumn(MaterialDesignIcon icon, Function<S, S> function) {
         return param -> new ButtonCell<>(icon, function);
     }
 
     @Override
-    public void updateItem(Button item, boolean empty) {
+    public void updateItem(JFXButton item, boolean empty) {
         super.updateItem(item, empty);
 
         if (empty) {
--- art/src/main/java/ui/main/patterns/PatternsController.java
+++ art/src/main/java/ui/main/patterns/PatternsController.java
@@ -1,17 +1,20 @@
 package ui.main.patterns;
 
+import com.jfoenix.controls.JFXButton;
+import data.entities.AccessCondition;
 import data.entities.AccessPattern;
 
-import data.entities.Whitelist;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 
 import io.msoffice.excel.AccessPatternImportHelper;
 
 import java.util.List;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -21,36 +24,29 @@ import ui.custom.controls.ButtonCell;
 public class PatternsController {
 
     @FXML
-    public TableView patternsTable;
+    public TableView<AccessPattern> patternsTable;
 
     @FXML
-    public TableColumn deleteColumn;
+    public TableColumn<AccessPattern, List<AccessCondition>> useCaseCountColumn;
 
     @FXML
-    public TableColumn editColumn;
+    public TableColumn<AccessPattern, JFXButton> deleteColumn;
+
+    @FXML
+    public TableColumn<AccessPattern, JFXButton> editColumn;
 
     /**
      * Initializes the controller.
      */
     @FXML
     public void initialize() {
-        deleteColumn.setCellFactory(ButtonCell.forTableColumn(MaterialDesignIcon.DELETE, (AccessPattern accessPattern) -> {
-            patternsTable.getItems().remove(accessPattern);
-            return accessPattern;
-        }));
-        deleteColumn.getStyleClass().add("undecorated-column");
+        initializeTableColumns();
 
-        editColumn.setCellFactory(ButtonCell.forTableColumn(MaterialDesignIcon.PENCIL, (AccessPattern accessPattern) -> {
-            editAccessPattern(accessPattern);
-            return accessPattern;
-        }));
-        editColumn.getStyleClass().add("undecorated-column");
-
-        AccessPatternImportHelper helper = new AccessPatternImportHelper();
-
+        // test the table with data from the Example - Zugriffsmuster.xlsx file
         try {
-            List<AccessPattern> patterns = helper.importAuthorizationPattern("Example - Zugriffsmuster.xlsx");
+            AccessPatternImportHelper helper = new AccessPatternImportHelper();
 
+            List<AccessPattern> patterns = helper.importAuthorizationPattern("Example - Zugriffsmuster.xlsx");
             ObservableList<AccessPattern> list = FXCollections.observableList(patterns);
 
             patternsTable.setItems(list);
@@ -58,6 +54,40 @@ public class PatternsController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Initializes the table columns that need extra content.
+     */
+    private void initializeTableColumns() {
+
+        // Add the delete column
+        deleteColumn.setCellFactory(ButtonCell.forTableColumn(MaterialDesignIcon.DELETE, (AccessPattern accessPattern) -> {
+            patternsTable.getItems().remove(accessPattern);
+            return accessPattern;
+        }));
+        deleteColumn.getStyleClass().add("undecorated-column");
+
+        // Add the edit column
+        editColumn.setCellFactory(ButtonCell.forTableColumn(MaterialDesignIcon.PENCIL, (AccessPattern accessPattern) -> {
+            editAccessPattern(accessPattern);
+            return accessPattern;
+        }));
+        editColumn.getStyleClass().add("undecorated-column");
+
+        // overwrite the column in which the number of useCases is displayed
+        useCaseCountColumn.setCellFactory(col -> new TableCell<AccessPattern, List<AccessCondition>>() {
+            @Override
+            protected void updateItem(List<AccessCondition> items, boolean empty) {
+                if (empty || items == null) {
+                    setText("" + 0);
+                } else {
+                    // set image for non-empty cell
+                    setText("" + items.size());
+                }
+            }
+
+        });
     }
 
     public void editAccessPattern(AccessPattern accessPattern) {
