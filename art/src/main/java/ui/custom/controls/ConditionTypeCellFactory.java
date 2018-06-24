ddde356f65945ca51dbdcb07c655374d7890bcd3
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-06-24 22:40:24 +0200

    ART-8 fixed ConditionTypeCellFactory display bug

--- art/src/main/java/ui/custom/controls/ConditionTypeCellFactory.java
+++ art/src/main/java/ui/custom/controls/ConditionTypeCellFactory.java
@@ -35,6 +35,7 @@ public class ConditionTypeCellFactory implements Callback<TableColumn<AccessPatt
 
                     // nothing to display
                     setText("");
+                    setGraphic(null);
 
                 } else {
 
