8d047bd24fa529d9193c0187feccca322f8ee354
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-06-27 14:47:20 +0200

    ART-59 handle user access restrictions

--- art/src/main/java/ui/main/MainController.java
+++ art/src/main/java/ui/main/MainController.java
@@ -1,7 +1,11 @@
 package ui.main;
 
 import com.jfoenix.controls.JFXTabPane;
+import data.entities.DbUser;
+import data.entities.DbUserRole;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import ui.AppComponents;
 import ui.main.admin.AdminController;
 import ui.main.configs.ConfigsController;
 import ui.main.patterns.PatternsController;
@@ -30,13 +34,34 @@ public class MainController {
     private WhitelistsController whitelistsController;
 
     @FXML
-    private AdminController adminController;
+    private AdminController usersController;
+
+    @FXML
+    private Tab sapQueriesTab;
+
+    @FXML
+    private Tab sapSettingsTab;
+
+    @FXML
+    private Tab configurationsTab;
+
+    @FXML
+    private Tab patternsTab;
+
+    @FXML
+    private Tab whitelistsTab;
+
+    @FXML
+    private Tab usersTab;
+
 
     /**
      * Initializes the controller.
      */
     @FXML
     public void initialize() throws Exception {
+
+        initializeAccessRestriction();
 
         updateAllTables();
 
@@ -51,6 +76,34 @@ public class MainController {
     }
 
     /**
+     * Adds only the tabs that the user roles can access.
+     */
+    private void initializeAccessRestriction() throws Exception {
+
+        // clear the tabs
+        mainTabs.getTabs().clear();
+
+        // get the current user
+        DbUser currentUser = AppComponents.getInstance().getDbContext().getCurrentUser();
+
+        // get the user roles
+        boolean analyst = currentUser.getRoles().contains(DbUserRole.DataAnalyst);
+        boolean viewer = currentUser.getRoles().contains(DbUserRole.Viewer);
+        boolean admin = currentUser.getRoles().contains(DbUserRole.Admin);
+
+        // add the tabs according to the role
+        if (analyst || viewer) {
+            mainTabs.getTabs().add(sapQueriesTab);
+        }
+        if (analyst) {
+            mainTabs.getTabs().addAll(sapSettingsTab, configurationsTab, patternsTab, whitelistsTab);
+        }
+        if (admin) {
+            mainTabs.getTabs().add(usersTab);
+        }
+    }
+
+    /**
      * Update all tables (maybe find a more efficient way).
      */
     private void updateAllTables() throws Exception {
@@ -59,5 +112,6 @@ public class MainController {
         configsController.updateTable();
         patternsController.updateTable();
         whitelistsController.updateTable();
+        usersController.updateTable();
     }
 }
