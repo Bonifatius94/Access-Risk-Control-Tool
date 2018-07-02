4f2fd43730ba028af0fec1e0835526847042bfeb
Marco Tröster <marco.troester@student.uni-augsburg.de>
2018-07-02 18:00:14 +0200

    - made some changes to deletePattern(), but it is not working yet

--- art/src/main/java/data/localdb/ArtDbContext.java
+++ art/src/main/java/data/localdb/ArtDbContext.java
@@ -1160,16 +1160,24 @@ public class ArtDbContext extends H2ContextBase implements IArtDbContext {
 
                 transaction = session.beginTransaction();
 
-                // remove pattern from active configs referencing it
-                pattern.getConfigurations().forEach(config -> {
+                Set<Configuration> configsToArchive = new HashSet<>(pattern.getConfigurations());
+
+                // remove pattern from active configs referencing it and archive those configs
+                for (Configuration config : configsToArchive) {
 
                     if (!config.isArchived()) {
 
-                        config.getPatterns().remove(pattern);
-                        config.adjustReferences();
+                        Set<AccessPattern> patterns = new HashSet<>(config.getPatterns());
+                        patterns.stream().filter(x -> x.getId().equals(pattern.getId())).collect(Collectors.toList()).forEach(x -> patterns.remove(x));
+                        config.setPatterns(patterns);
+
+                        if (archive) {
+                            archiveConfig(session, config);
+                        }
+
                         session.update(config);
                     }
-                });
+                }
 
                 if (archive) {
 
@@ -1225,12 +1233,22 @@ public class ArtDbContext extends H2ContextBase implements IArtDbContext {
 
                 transaction = session.beginTransaction();
 
-                // remove whitelist from active configs referencing it
-                getConfigs(false).forEach(config -> {
+                List<Configuration> configsToArchive =
+                    getConfigs(false).stream()
+                    .filter(x -> x.getWhitelist() != null && x.getWhitelist().getId().equals(whitelist.getId()))
+                    .collect(Collectors.toList());
+
+                // remove whitelist from active configs referencing it and archive those configs
+                for (Configuration config : configsToArchive) {
 
                     config.setWhitelist(null);
+
+                    if (archive) {
+                        archiveConfig(session, config);
+                    }
+
                     session.update(config);
-                });
+                }
 
                 if (archive) {
 
