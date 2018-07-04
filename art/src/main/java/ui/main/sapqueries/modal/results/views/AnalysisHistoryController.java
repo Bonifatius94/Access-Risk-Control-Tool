95f37cae99236a4f8198c25cab8a6b5d43c2a898
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-07-04 15:46:05 +0200

    ART-82 added additional series to the chart

--- art/src/main/java/ui/main/sapqueries/modal/results/views/AnalysisHistoryController.java
+++ art/src/main/java/ui/main/sapqueries/modal/results/views/AnalysisHistoryController.java
@@ -1,12 +1,20 @@
 package ui.main.sapqueries.modal.results.views;
 
+import com.jfoenix.controls.JFXComboBox;
+import data.entities.AccessPattern;
+import data.entities.CriticalAccessEntry;
 import data.entities.CriticalAccessQuery;
 
+import data.entities.Whitelist;
 import extensions.ResourceBundleHelper;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
@@ -15,6 +23,8 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
 
 public class AnalysisHistoryController {
 
@@ -27,8 +37,24 @@ public class AnalysisHistoryController {
     @FXML
     private NumberAxis chartY;
 
+    @FXML
+    private VBox additionalDataBox;
+
+    @FXML
+    private JFXComboBox<CriticalAccessEntry> data0Box;
+
+    @FXML
+    private JFXComboBox<CriticalAccessEntry> data1Box;
+
+    @FXML
+    private JFXComboBox<CriticalAccessEntry> data2Box;
+
     private List<CriticalAccessQuery> relatedQueries;
     private CriticalAccessQuery query;
+
+    private XYChart.Series<String, Integer> additionalSeries0;
+    private XYChart.Series<String, Integer> additionalSeries1;
+    private XYChart.Series<String, Integer> additionalSeries2;
 
     private ResourceBundle bundle = ResourceBundleHelper.getInstance().getLanguageBundle();
 
@@ -48,6 +74,7 @@ public class AnalysisHistoryController {
             createChart();
         }
 
+        initializeAdditionalDataBox();
     }
 
     /**
@@ -77,6 +104,7 @@ public class AnalysisHistoryController {
         queriesHistoryChart.getData().add(mainSeries);
 
         showValuesOnHover();
+
     }
 
     /**
@@ -92,6 +120,134 @@ public class AnalysisHistoryController {
                     d.getNode().getStyleClass().add("current-node");
                 }
             }
+        }
+    }
+
+    /**
+     * Initialize the additional data boxes.
+     */
+    private void initializeAdditionalDataBox() {
+
+        data0Box.setConverter(new CriticalAccessEntryStringConverter());
+        data1Box.setConverter(new CriticalAccessEntryStringConverter());
+        data2Box.setConverter(new CriticalAccessEntryStringConverter());
+
+        List<CriticalAccessEntry> allEntries = new ArrayList<>();
+
+        for (CriticalAccessQuery query : relatedQueries) {
+            allEntries.addAll(query.getEntries());
+        }
+
+        allEntries = allEntries.stream().distinct().collect(Collectors.toList());
+
+        data0Box.getItems().addAll(allEntries);
+        data1Box.getItems().addAll(allEntries);
+        data2Box.getItems().addAll(allEntries);
+
+        data0Box.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addSeries0ToChart(newValue);
+            }
+        }));
+
+        data1Box.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addSeries1ToChart(newValue);
+            }
+        }));
+
+        data2Box.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addSeries2ToChart(newValue);
+            }
+        }));
+    }
+
+    /**
+     * Add the series0 to the chart.
+     */
+    private void addSeries0ToChart(CriticalAccessEntry entry) {
+
+        queriesHistoryChart.getData().remove(additionalSeries0);
+
+        additionalSeries0 = new XYChart.Series<>();
+        additionalSeries0.setName(bundle.getString("numberOfViolations"));
+
+        for (CriticalAccessQuery query : relatedQueries) {
+
+            int result = (int) query.getEntries().stream().map(x -> x.getAccessPattern().getUsecaseId()).filter(y -> y.equals(entry.getAccessPattern().getUsecaseId())).count();
+
+            additionalSeries0.getData().add(new XYChart.Data<>(query.getCreatedAt().format(DateTimeFormatter.ofPattern("dd.MM.yyyy\n    HH:mm")), result));
+        }
+
+        queriesHistoryChart.getData().add(additionalSeries0);
+    }
+
+    /**
+     * Add the series1 to the chart.
+     */
+    private void addSeries1ToChart(CriticalAccessEntry entry) {
+
+        queriesHistoryChart.getData().remove(additionalSeries1);
+
+
+        additionalSeries1 = new XYChart.Series<>();
+        additionalSeries1.setName(bundle.getString("numberOfViolations"));
+
+        for (CriticalAccessQuery query : relatedQueries) {
+
+            int result = (int) query.getEntries().stream().map(x -> x.getAccessPattern().getUsecaseId()).filter(y -> y.equals(entry.getAccessPattern().getUsecaseId())).count();
+
+            additionalSeries1.getData().add(new XYChart.Data<>(query.getCreatedAt().format(DateTimeFormatter.ofPattern("dd.MM.yyyy\n    HH:mm")), result));
+        }
+
+        queriesHistoryChart.getData().add(additionalSeries1);
+    }
+
+    /**
+     * Add the series2 to the chart.
+     */
+    private void addSeries2ToChart(CriticalAccessEntry entry) {
+
+        queriesHistoryChart.getData().remove(additionalSeries2);
+
+
+        additionalSeries2 = new XYChart.Series<>();
+        additionalSeries2.setName(bundle.getString("numberOfViolations"));
+
+        for (CriticalAccessQuery query : relatedQueries) {
+
+            int result = (int) query.getEntries().stream().map(x -> x.getAccessPattern().getUsecaseId()).filter(y -> y.equals(entry.getAccessPattern().getUsecaseId())).count();
+
+            additionalSeries2.getData().add(new XYChart.Data<>(query.getCreatedAt().format(DateTimeFormatter.ofPattern("dd.MM.yyyy\n    HH:mm")), result));
+        }
+
+        queriesHistoryChart.getData().add(additionalSeries2);
+    }
+
+
+    /**
+     * WhitelistStringConverter for the whitelistChooser ComboBox.
+     */
+    private class CriticalAccessEntryStringConverter extends StringConverter<CriticalAccessEntry> {
+        private Map<String, CriticalAccessEntry> map = new HashMap<>();
+
+        @Override
+        public String toString(CriticalAccessEntry criticalAccessEntry) {
+            if (criticalAccessEntry == null) {
+                return "";
+            }
+            String str = criticalAccessEntry.getAccessPattern().getUsecaseId();
+            map.put(str, criticalAccessEntry);
+            return str;
+        }
+
+        @Override
+        public CriticalAccessEntry fromString(String string) {
+            if (!map.containsKey(string)) {
+                return null;
+            }
+            return map.get(string);
         }
     }
 }
