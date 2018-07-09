056fb190c12f527097a01c9d9021758082293943
Merlin Albes <merlin.albes@student.uni-augsburg.de>
2018-07-09 11:12:56 +0200

    fixed small import bug where admin and viewer could import patterns / whitelists

--- art/src/main/java/ui/main/MainController.java
+++ art/src/main/java/ui/main/MainController.java
@@ -91,8 +91,6 @@ public class MainController {
         initTableUpdates();
 
         updateAllTables();
-
-        initializeDragAndDrop();
     }
 
     /**
@@ -116,6 +114,7 @@ public class MainController {
             mainTabs.getTabs().add(sapQueriesTab);
         }
         if (analyst) {
+            initializeDragAndDrop();
             mainTabs.getTabs().addAll(sapSettingsTab, configurationsTab, patternsTab, whitelistsTab);
         }
         if (admin) {
