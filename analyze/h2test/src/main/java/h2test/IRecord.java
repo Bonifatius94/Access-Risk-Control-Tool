fa5c696d00d2d7d2ffe25189545629e4270189b9
Marco Tröster <marco@troester-gmbh.de>
2022-02-04 14:10:36 +0100

    remove tech research region with prototype samples

--- analyze/h2test/src/main/java/h2test/IRecord.java
+++
@@ -1,7 +0,0 @@
-package h2test;
-
-public interface IRecord {
-
-    // nothing needs to be added here ...
-    // add a method stub here if there are methods that need to be implemented by each DAO class
-}
